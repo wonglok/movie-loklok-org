98b7b9cb40d309645745679e438fe0d84e851866
Lok Lok <yellowhappy831@gmail.com>
2026-06-15 14:20:23 +0800

    save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,28 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "video.loklok.org",
   description: "video.loklok.org",
+
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
@@ -35,3 +57,11 @@ export default function RootLayout({
     </html>
   );
 }
+
+//
+
+//
+
+//
+
+//
